2e2f847137050dd04c7d76cda6dfc36d5f143ee2
engineer2006-2028 <behruzabdurasulov185@gmail.com>
2026-03-09 22:22:28 +0100

    Add files via upload

---
+++ even or odd/src/numbers.java
@@ -0,0 +1,9 @@
+public class numbers {
+
+     static void main(String[]args) {
+             int number = (int) (Math.random() * 100) + 1;
+             if (number % 2 == 0) {
+             System.out.println("even "+number);}
+             else{System.out.println("odd " +number);}
+             }
+         }
